8f3229a29e31554326ca13303c874be29732ab87
steph <stephieoldcorn@gmail.com>
2014-12-01 12:13:45 +0000

    Adding comments to the directory

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+#first, we print out the list of students
 puts "The students of my cohort at Makers Academy"
 puts "------------"
 puts "Sanda Golcea"
@@ -5,6 +6,8 @@ puts "Matteo Manzo"
 puts "Huy Le"
 puts "David Blake"
 
+#finally, we print the total
 print "Overall, we have "
+#print does not add a new line character
 print 4
 puts " great students"
