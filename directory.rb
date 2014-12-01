8a91b82b7b0dc38b71036d4d9c01af16c5f62e37
steph <stephieoldcorn@gmail.com>
2014-12-01 14:39:10 +0000

    Getting input from the user

--- directory.rb
+++ directory.rb
@@ -1,10 +1,22 @@
-#puts the students into an array
-students = [
-{:name => "Sanda Golcea", :cohort => :december},
-{:name => "Matteo Manzo", :cohort => :december},
-{:name => "Huy Le", :cohort => :december},
-{:name => "David Blake", :cohort => :december}
-]
+def input_students
+	puts "Please enter the names of the students."
+	puts "To finish, just hit return twice."
+	#create an empty array
+	students = []
+	#get the first name
+	name = gets.chomp
+	#while the name is not empty, repeat this code
+	while !name.empty? do
+		#add the student hash to the array
+		students << {:name => name, :cohort => :december}
+		puts "Now we have #{students.length} students."
+		#get another name from the user
+		name = gets.chomp
+	end
+	#return the array of students
+	students
+end
+
 
 def print_header
 	puts "The students of my cohort at Makers Academy"
@@ -22,6 +34,7 @@ def print_footer(students)
 end
 
 #calling the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
