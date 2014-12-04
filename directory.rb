ad051150d73e8d4c0a7ca7fca0c8b721954c6cc3
steph <stephieoldcorn@gmail.com>
2014-12-04 12:52:28 +0000

    Refactoring and fixing cohort grouping

--- directory.rb
+++ directory.rb
@@ -1,73 +1,24 @@
 @students = []
+@cohort_list = []
 
-def input_students
-	puts "Please enter the name of the student."
-	puts "To finish, just hit return twice."
-	#get the first name
-	name = STDIN.gets.chomp
-if !name.empty?
-	#while the name is not empty, repeat this code
-	while !name.empty? do
-		#add the student hash to the array
-		print "Cohort: "
-		cohort = STDIN.gets.chomp
-		cohort = "Not provided" if cohort.empty?
-		print "Date of birth dd/mm/yyyy: "
-		dob = STDIN.gets.chomp
-		dob = "Not provided" if dob.empty?
-		print "Hometown: "
-		hometown = STDIN.gets.chomp
-		hometown = "Not provided" if hometown.empty?
-		add_student(name, cohort, dob, hometown)
-		if @students.length == 1
-			puts "Now we have #{@students.length} student, please enter the next name or hit return"
-		else
-			puts "Now we have #{@students.length} students, please enter the next name or hit return"
-		end
-		#get another name from the user
-		name = STDIN.gets.chomp
-	end
-	#return the array of students
-	@students
-else
-	input_students
-end
-end
-
-
-def print_header
-	puts "The students of the cohorts at Makers Academy".center(50)
-	puts "------------".center(50)
-end
-
-def print_students_list
-	def cohorts(x)
-	month = @students.select {|student| student[:cohort] == x.to_sym}
-	if !month.empty? && x == "Not provided"
-			puts "The following students do not belong to a cohort yet:"
-			month.map.each_with_index{|student, index| puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort), DoB: #{student[:dob]}, Hometown: #{student[:hometown]}\n".lstrip}
-			puts ""
-	elsif !month.empty? 
-			puts "The #{x} cohort:"
-			month.map.each_with_index{|student, index| puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort), DoB: #{student[:dob]}, Hometown: #{student[:hometown]}\n".lstrip}
-			puts ""
+def try_load_students
+	filename = ARGV.first
+	return if filename.nil?
+	if File.exists?(filename)
+		load_students(filename)
+		puts "Loaded #{@students.length} from #{filename}"
 	else
-	print ""
-	end	
-	end
-cohorts("December")	
-cohorts("January")	
-cohorts("Not provided")	
-end
-
-def print_footer
-	if @students.length == 1
-		puts "Overall, we have #{@students.length} great student".center(50)
-	else
-		puts "Overall, we have #{@students.length} great students".center(50)
+		puts "Sorry, #{filename} does not exist"
+		exit
 	end
 end
 
+def interactive_menu
+	loop do
+		print_menu
+		process(STDIN.gets.chomp)
+	end
+end
 
 def print_menu
 	puts "1. Input the students"
@@ -75,12 +26,6 @@ def print_menu
 	puts "3. Save the list to students.csv"
 	puts "4. Load the list from students.csv"
 	puts "9. Exit"
-end
-
-def show_students
-	print_header
-	print_students_list
-	print_footer
 end
 
 def process(selection)
@@ -100,21 +45,88 @@ def process(selection)
 	end
 end
 
-def interactive_menu
-	loop do
-		print_menu
-		process(STDIN.gets.chomp)
+def add_student(name, cohort, dob, hometown)
+	@students << {:name => name, :cohort => cohort.to_sym, :dob => dob, :hometown => hometown}
+end
+
+def display_input_message
+	if @students.length == 1
+		puts "Now we have #{@students.length} student, please enter the next name or hit return"
+	else
+		puts "Now we have #{@students.length} students, please enter the next name or hit return"
+	end
+end
+
+def input_students
+	puts "Please enter the name of the student."
+	puts "To finish, just hit return twice."
+	name = STDIN.gets.chomp
+	if !name.empty?
+		while !name.empty? do
+			print "Cohort: "
+			cohort = STDIN.gets.chomp
+			cohort = "Not provided" if cohort.empty?
+			print "Date of birth dd/mm/yyyy: "
+			dob = STDIN.gets.chomp
+			dob = "Not provided" if dob.empty?
+			print "Hometown: "
+			hometown = STDIN.gets.chomp
+			hometown = "Not provided" if hometown.empty?
+			add_student(name, cohort, dob, hometown)
+			display_input_message
+			name = STDIN.gets.chomp
+		end
+		@students
+	else
+		input_students
+	end
+end
+
+def show_students
+	print_header
+	print_students_list
+	print_footer
+end
+
+def print_header
+	puts "The students of the cohorts at Makers Academy".center(50)
+	puts "------------".center(50)
+end
+
+def existing_cohorts
+	@students.select {|student| @cohort_list << student[:cohort] unless @cohort_list.include?(student[:cohort])}
+	@cohort_list
+end
+
+def print_students_list
+	existing_cohorts.each do |x|
+		month = @students.select {|student| student[:cohort] == x}
+		if x.to_s == "Not provided"
+			puts "The following students do not belong to a cohort yet:"
+			month.map.each_with_index{|student, index| puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort), DoB: #{student[:dob]}, Hometown: #{student[:hometown]}\n".lstrip}
+			puts ""
+		else 
+			puts "The #{x} cohort:"
+			month.map.each_with_index{|student, index| puts "#{index+1}. #{student[:name]} (#{student[:cohort]} cohort), DoB: #{student[:dob]}, Hometown: #{student[:hometown]}\n".lstrip}
+			puts ""
+		end
+	end	
+end
+
+def print_footer
+	if @students.length == 1
+		puts "Overall, we have #{@students.length} great student".center(50)
+	else
+		puts "Overall, we have #{@students.length} great students".center(50)
 	end
 end
 
 def save_students
-	#open the file for writing
 	file = File.open("students.csv", "w")
-	#iterate over the array of students
 	@students.each do |student|
-	student_data = [student[:name], student[:cohort], student[:dob], student[:hometown]]
-	csv_line = student_data.join(",")
-	file.puts csv_line
+		student_data = [student[:name], student[:cohort], student[:dob], student[:hometown]]
+		csv_line = student_data.join(",")
+		file.puts csv_line
 	end
 	file.close
 end
@@ -122,29 +134,12 @@ end
 def load_students(filename = "students.csv")
 	file = File.open(filename, "r")
 	file.readlines.each do |line|
-	name, cohort, dob, hometown = line.chomp.split(',')
-	add_student(name, cohort, dob, hometown)
+		name, cohort, dob, hometown = line.chomp.split(',')
+		add_student(name, cohort, dob, hometown)
 	end
 	file.close
 end
 
-def try_load_students
-	filename = ARGV.first
-	return if filename.nil?
-	if File.exists?(filename)
-		load_students(filename)
-		puts "Loaded #{@students.length} from #{filename}"
-	else
-		puts "Sorry, #{filename} does not exist"
-		exit
-	end
-end
-
-def add_student(name, cohort, dob, hometown)
-	@students << {:name => name, :cohort => cohort.to_sym, :dob => dob, :hometown => hometown}
-end
-
-#calling the methods
 try_load_students
 interactive_menu
 
